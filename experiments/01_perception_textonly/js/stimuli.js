b7fcb83c4be2b4f7cdb99f87871d7455be5964e7
Morgan Moyer <mcmoyer11@gmail.com>
2024-09-27 14:03:42 +0200

    updated github raw stim link

--- experiments/01_perception_textonly/js/stimuli.js
+++ experiments/01_perception_textonly/js/stimuli.js
@@ -14,35 +14,6 @@ function get_data(fullpath) {
     return response.responseText;
 }
 
-// function generate_stim() {
-
-//     var contents = get_data(path);
-//     var raw = contents;
-//     var rows = raw.split('\n');
-//     var data = [];
-
-//     headings = rows[0].split('\t');
-//     var total = rows.length - 1;
-
-//     // Push every item from the text file into the array, split on the tab (or whatever split)
-//     for (var i = 1; i < total; i++) {
-//         data.push(rows[i].split('\t'));
-//     }
-
-//     console.log("data: ", data);
-
-//     var stim = data.map(function (row) {
-//         return row.reduce(function (result, field, index) {
-//             result[headings[index]] = field;
-//             return result;
-//         }, {});
-//     });
-
-//     console.log("stim: ", stim);
-
-//     return stim;
-
-// }
 
 function generate_stim() {
     var contents = get_data(path);
@@ -72,3 +43,33 @@ function generate_stim() {
 
     return data;
 }
+
+// function generate_stim() {
+
+//     var contents = get_data(path);
+//     var raw = contents;
+//     var rows = raw.split('\n');
+//     var data = [];
+
+//     headings = rows[0].split('\t');
+//     var total = rows.length - 1;
+
+//     // Push every item from the text file into the array, split on the tab (or whatever split)
+//     for (var i = 1; i < total; i++) {
+//         data.push(rows[i].split('\t'));
+//     }
+
+//     console.log("data: ", data);
+
+//     var stim = data.map(function (row) {
+//         return row.reduce(function (result, field, index) {
+//             result[headings[index]] = field;
+//             return result;
+//         }, {});
+//     });
+
+//     console.log("stim: ", stim);
+
+//     return stim;
+
+// }
